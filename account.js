2972c8c2ff91ab7a5303d4c3ef2198540f656b4d
Douglas <douglaskerins@gmail.com>
2021-11-24 09:46:31 +0000

    adding passing getStatement test

--- account.js
+++ account.js
@@ -8,11 +8,44 @@ class Account {
       ammount: ammount
     })
   }
+
   withdrawal(ammount, date) {
     this.transactions.push({
       date: date,
       ammount: -ammount
     })
+  }
+
+  getStatement() {
+    console.log('date || credit || debit || balance');
+    let balance = 0;
+    let statement = this.transactions.map(transaction => {
+      balance = this.getNewBalance(balance, transaction.ammount);
+      return (`${transaction.date} || ${this.getCreditAmmount(transaction.ammount)} || ${this.getDebitAmmount(transaction.ammount)} || ${balance}`);
+    })
+    console.log(statement);
+    return statement;
+  }
+
+  getDebitAmmount(ammount) {
+    if (ammount > 0) {
+      return "";
+    } else {
+      return Math.abs(ammount);
+    }
+
+  }
+
+  getCreditAmmount(ammount) {
+    if (ammount > 0) {
+      return ammount;
+    } else {
+      return "";
+    }
+  }
+
+  getNewBalance(currentBalance, transaction) {
+    return currentBalance + transaction
   }
 }
 
--- account.test.js
+++ account.test.js
@@ -33,5 +33,16 @@ describe('Account', () => {
       ]);
     })
   })
+
+  describe('getStatement()', () => {
+    it('should allow user to view a statement of their transactions and balances', () => {
+      const account = new Account;
+      transactionDate = new Date(2021 - 11 - 5);
+      account.deposit(100, transactionDate);
+      expect(account.getStatement()).toEqual([
+        'Thu Jan 01 1970 01:00:02 GMT+0100 (Greenwich Mean Time) || 100 ||  || 100'
+      ]);
+    })
+  })
 })
 
