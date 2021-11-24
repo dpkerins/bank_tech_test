be8a03e1b21353849152c17c7e5f916c12049222
Douglas <douglaskerins@gmail.com>
2021-11-24 08:32:07 +0000

    adding passing deposit test

--- account.js
+++ account.js
@@ -2,6 +2,13 @@ class Account {
   constructor() {
     this.transactions = [];
   }
+  deposit(ammount, date) {
+    this.transactions.push({
+      date: date,
+      ammount: ammount,
+      type: 'credit'
+    })
+  }
 }
 
 module.exports = Account;
--- account.test.js
+++ account.test.js
@@ -5,5 +5,19 @@ describe('Account', () => {
     const account = new Account;
     expect(account.transactions).toEqual([]);
   })
+  describe('deposit()', () => {
+    it('should allow user to create a deposit transaction', () => {
+      const account = new Account;
+      transactionDate = new Date(2021 - 11 - 1);
+      account.deposit(100, transactionDate);
+      expect(account.transactions).toEqual([
+        {
+          date: transactionDate,
+          type: 'credit',
+          ammount: 100
+        }
+      ]);
+    })
+  })
 })
 
